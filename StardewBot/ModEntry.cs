8c31130e98865baf1891573b724fdcfcf04ac2c1
Evan Fredericksen <evfredericksen@gmail.com>
2020-12-31 17:27:46 -0500

    first commit

---
+++ StardewBot/ModEntry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace StardewBot
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+        
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                //return;
+            // print button presses to the console window
+            this.Monitor.Log($"{Game1.player.Name} presseddd {e.Button}.", LogLevel.Debug);
+            this.Monitor.Log(e.Button.ToString(), LogLevel.Debug);
+            
+            if (e.Button.ToString() == "P") {
+                this.Monitor.Log("pressed p");
+            }
+        }
+    }
+}
