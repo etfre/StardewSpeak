3bd2d8c31882ca558421c7008a26c9d71ac60ada
Evan Fredericksen <evfredericksen@gmail.com>
2021-01-26 17:40:38 -0500

    possessive locations

--- StardewSpeak/lib/speech-client/speech-client/locations.py
+++ StardewSpeak/lib/speech-client/speech-client/locations.py
@@ -1,16 +1,22 @@
 class Location:
 
-    def __init__(self, name: str, commands=None):
+    def __init__(self, name: str, commands=None, possessive=False):
         self.name = name
         self.commands = [self.name.lower()] if commands is None else commands
+        self.possessive = possessive
 
 def init_locations():
     return (
+        Location("Backwoods"),
         Location("BusStop", ["bus stop"]),
         Location("Farm"),
         Location("FarmHouse", ["farm house"]),
-        Location("LeahHouse", ["leah's house"]),
+        Location("Forest"),
+        Location("LeahHouse", ["leah's house"], possessive=True),
+        Location("Mine", ["mine", "mines"]),
+        Location("Mountain"),
         Location("Town"),
+        Location("Woods"),
     )
 
 def location_commands(locs):
@@ -18,6 +24,8 @@ def location_commands(locs):
     for loc in locs:
         for cmd in loc.commands:
             assert cmd not in commands
+            if not loc.possessive:
+                cmd = f'[the] {cmd}'
             commands[cmd] = loc
     return commands
 
