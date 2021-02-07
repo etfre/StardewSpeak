c8f64e8cc6f3cc604234bdfc3407c80182c4cb6a
Evan Fredericksen <evfredericksen@gmail.com>
2021-02-07 15:28:43 -0500

    container menu refactor

--- StardewSpeak/lib/speech-client/speech-client/container_menu.py
+++ StardewSpeak/lib/speech-client/speech-client/container_menu.py
@@ -1,4 +1,5 @@
 import game, server, menu_utils
+import functools
 import dragonfly as df
 
 # item_grab_submenu_rule = df.Choice("direction_keys", direction_keys) 
@@ -17,9 +18,7 @@ def reset_item_grab_state():
 
 async def set_item_grab_submenu(submenu_name: str):
     assert submenu_name in ('inventoryMenu', 'itemsToGrabMenu')
-    menu = await menu_utils.get_active_menu()
-    if not menu or menu['menuType'] != 'itemsToGrabMenu':
-        raise menu_utils.InvalidMenuOption()
+    menu = await menu_utils.get_active_menu('itemsToGrabMenu')
     submenu = menu[submenu_name]
     if submenu['containsMouse']:
         return
@@ -35,30 +34,25 @@ async def focus_item(new_row, new_col):
     if menu['menuType'] == "itemsToGrabMenu":
         submenu_name = 'itemsToGrabMenu' if menu['itemsToGrabMenu']['containsMouse'] else 'inventoryMenu'
         submenu = menu[submenu_name]
-        def get_previous():
-            return item_grab[submenu_name]
-        def set_previous(r, c):
-            item_grab[submenu_name] = r, c
-        await focus_inventory_box(submenu['inventory'], new_row, new_col, get_previous, set_previous)
-        # rows = menu_utils.list_of_rows(submenu['inventory'])
-        # indices = menu_utils.find_component_containing_mouse(rows)
-        # row, col = indices if indices else item_grab[submenu_name]
-        # if new_row is not None:
-        #     row = new_row
-        # if new_col is not None:
-        #     col = new_col
-        # cmp = rows[row][col]
-        # await menu_utils.focus_component(cmp)
-        # item_grab[submenu_name] = row, col
+
+        await focus_inventory_box(
+            submenu['inventory'],
+            new_row, new_col,
+            get_previous=lambda: item_grab[submenu_name],
+            set_previous=functools.partial(set_previous_container, submenu_name),
+        )
 
 async def focus_inventory_box(cmp_list, new_row, new_col, get_previous=lambda: (0, 0), set_previous=lambda r, c: None):
     rows = menu_utils.list_of_rows(cmp_list)
-    indices = menu_utils.find_component_containing_mouse(rows)
-    row, col = indices if indices else get_previous()
-    if new_row is not None:
-        row = new_row
-    if new_col is not None:
-        col = new_col
+    indices = menu_utils.find_component_containing_mouse(rows) or get_previous()
+    row = indices[0] if new_row is None else new_row
+    col = indices[1] if new_col is None else new_col
     cmp = rows[row][col]
     await menu_utils.focus_component(cmp)
     set_previous(row, col)
+    
+def get_previous_container():
+    return item_grab[submenu_name]
+
+def set_previous_container(submenu_name, r, c):
+    item_grab[submenu_name] = r, c
