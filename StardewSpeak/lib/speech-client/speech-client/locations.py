ea614a609a864fb39efa5634466bcfcb4f9d5f58
Evan Fredericksen <evfredericksen@gmail.com>
2021-02-18 22:58:21 -0500

    add locations

--- StardewSpeak/lib/speech-client/speech-client/locations.py
+++ StardewSpeak/lib/speech-client/speech-client/locations.py
@@ -1,24 +1,43 @@
+import re
+
 class Location:
 
     def __init__(self, name: str, commands=None, possessive=False):
         self.name = name
-        self.commands = [self.name.lower()] if commands is None else commands
+        self.commands = self.format_name(name) if commands is None else commands
         self.possessive = possessive
+
+    def format_name(self, s: str):
+        capitals_split = re.findall('[A-Z][a-z]*', s)
+        return [x.lower() for x in capitals_split]
 
 def init_locations():
     return (
+        Location("AdventureGuild"),
         Location("Backwoods"),
+        Location("Beach"),
+        Location("Blacksmith"),
         Location("BusStop", ["bus stop"]),
+        Location("ElliotHouse", ["elliot's house"], possessive=True),
         Location("Farm"),
+        Location("FarmCave", ["farm cave"]),
         Location("FarmHouse", ["farm house"]),
+        Location("FishShop"),
         Location("Forest"),
         Location("HaleyHouse", ["haley's house"], possessive=True),
+        Location("Hospital"),
+        Location("Mountain"),
         Location("JoshHouse", ["josh's house"], possessive=True),
         Location("LeahHouse", ["leah's house"], possessive=True),
+        Location("ManorHouse", ["manor house", "[mayor] lewis' house"]),
         Location("Mine", ["mine", "mines"]),
         Location("Mountain"),
         Location("SamHouse", ["sam's house"], possessive=True),
+        Location("Saloon", ["[stardrop] saloon"]),
+        Location("ScienceHouse"),
+        Location("SeedShop", ["[the] seed shop", "pierre's [general] shop", "[pierre's] general shop"], possessive=True),
         Location("Town"),
+        Location("Trailer"),
         Location("Woods"),
     )
 
