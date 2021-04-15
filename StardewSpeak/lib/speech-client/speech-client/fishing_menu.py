0798efbbe63c6a6630ec6366e8aa8cd537c11109
Evan <evfredericksen@gmail.com>
2021-04-15 17:08:16 -0400

    fishing cleanup

--- StardewSpeak/lib/speech-client/speech-client/fishing_menu.py
+++ StardewSpeak/lib/speech-client/speech-client/fishing_menu.py
@@ -5,20 +5,20 @@ async def catch_fish():
     await server.request('CATCH_FISH')
 
 async def start_fishing():
-    with server.player_status_stream() as stream:
+    async with server.player_status_stream() as stream:
         await game.equip_item_by_name(constants.FISHING_ROD)
-        await cast_fishing_rod()
-        await wait_for_nibble()
+    async with server.tool_status_stream() as tss:
+        await cast_fishing_rod(tss)
+        await wait_for_nibble(tss)
 
-async def cast_fishing_rod():
-    async with server.tool_status_stream() as tss, game.press_and_release(constants.USE_TOOL_BUTTON):
+async def cast_fishing_rod(tss):
+    async with game.press_and_release(constants.USE_TOOL_BUTTON):
         await tss.wait(lambda t: t['isTimingCast'] and t['castingPower'] > 0.95, timeout=10)
 
-async def wait_for_nibble():
-    with server.tool_status_stream() as tss:
-        tool_status = await tss.wait(lambda t: t['isNibbling'] or not t['inUse'])
-        if tool_status['inUse']:
-            await game.press_key(constants.USE_TOOL_BUTTON)
-            await tss.wait(lambda t: t['isReeling'])
+async def wait_for_nibble(tss):
+    tool_status = await tss.wait(lambda t: t['isNibbling'] or not t['inUse'])
+    if tool_status['inUse']:
+        await game.press_key(constants.USE_TOOL_BUTTON)
+        await tss.wait(lambda t: t['isReeling'])
 
 
